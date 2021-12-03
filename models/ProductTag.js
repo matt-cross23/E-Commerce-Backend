07443beb1d7209d1c77fc22f2cbdc92dd497b494
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2021-12-03 13:15:25 -0500

    Update ProductTag.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,7 +7,25 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: { 
+      type: data.Type.INTEGER,
+      allowNULL: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+   product_id: {
+    type: dataType.INTEGER,
+    references: {
+    model: 'Product',
+    key: 'id'
+      }
   },
+   tag_id: {
+    type: data.Type.INTEGER,
+    references: {
+    model: 'Tag',
+    key: 'id'
+    },
   {
     sequelize,
     timestamps: false,
