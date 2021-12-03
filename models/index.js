4f0354e631e138b1e40f30a74f658da60818453f
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2021-12-03 13:49:49 -0500

    Add Associations Model

--- models/index.js
+++ models/index.js
@@ -5,13 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-
+Categories.hasMany(Products, {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
+Products.belongsToMany(ProductTag, {
+  foreignKey: 'tag_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
-
+Tags.belongToMany(Products, {
+  foreignKey: 'tag_id'
+});
 module.exports = {
   Product,
   Category,
